a46c943e2efd1b0c3067b5bd81c6a033288669f8
arunkumar-1310 <arunkumar@cloudi5.com>
2024-12-04 13:15:38 +0530

    form corrections

--- application/views/frontend/portfolio-new.php
+++ application/views/frontend/portfolio-new.php
@@ -37,69 +37,8 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>asset/css/icon-font.css">
 
 <body id="cont1">
+
     <?php include('header.php'); ?>
-    <!-- <div class="banner-area about-banner resp-banner" id="banner-area" alt="Portfolio">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col">
-                    <div class="banner-heading">
-                        <h1 class="banner-title2">Portfolio</h1>
-                        <ol class="breadcrumb">
-                            <li><a href="<?php echo base_url(); ?>">Home</a></li>
-                            <li>Portfolio</li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
-    <!-- Portfolio -->
-    <!-- <section>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 col-sm-12">
-                    <section class="projects-page-section">
-                        <div class="auto-container">
-                            <div class="mixitup-gallery">
-                                <div class="filters clearfix">
-                                    <ul class="filter-tabs filter-btns text-center clearfix">
-                                        <li class="filter active" data-role="button" data-filter="all">All</li>
-                                        <?php foreach ($category as $res_category) { ?>
-                                            <li class="filter" data-role="button" data-filter=".<?php echo strtolower(str_replace(' ', '_', $res_category->title)); ?>">
-                                                <?php echo ucwords($res_category->title); ?></li>
-                                        <?php } ?>
-                                    </ul>
-                                </div>
-                                <div class="filter-list row clearfix" id="MixItUpC72ED9">
-                                    <?php foreach ($portfolio as $res_portfolio) { ?>
-                                        <div class="gallery-item-two mix <?php echo strtolower(str_replace(' ', '_', $res_portfolio->category)); ?> col-lg-4 col-md-6 col-sm-12 d-inlineb">
-                                            <div class="inner-box">
-                                                <figure class="image-box">
-                                                    <img src="<?php echo base_url() . $res_portfolio->thumbnail; ?>" alt="">
-                                                    <div class="overlay-box">
-                                                        <div class="overlay-inner">
-                                                            <div class="content">
-                                                                <a href="<?php echo base_url() . $res_portfolio->thumbnail_popup; ?>" data-fancybox="gallery-3" data-caption="" class="link"><span class="fa fa-search-plus"></span></a>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </figure>
-                                                <div class="lower-box">
-                                                    <h3><a href="<?php echo base_url() . 'portfolio/' . $res_portfolio->slug . '-' . $res_portfolio->id; ?>">
-                                                            <?php echo ucwords($res_portfolio->title); ?></a></h3>
-                                                    <div class="category"><?php echo $res_portfolio->services; ?></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    <?php } ?>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                </div>
-            </div>
-        </div>
-    </section> -->
 
     <section class="portfolio-new-section">
         <div class="container">
@@ -139,8 +78,11 @@
                                     <a href="javascript:void(0);">Tech Capabilities</a>
                                 </div>
                             </div>
-                            <h4 class="title">Delivering Exceptional Apartments & Villas with Unparalleled Quality in
-                                Coimbatore</h4>
+                            <a href="javascript:void(0);">
+                                <h4 class="title">Delivering Exceptional Apartments & Villas with Unparalleled Quality
+                                    in
+                                    Coimbatore</h4>
+                            </a>
                         </div>
                     </a>
                 </div>
@@ -154,15 +96,18 @@
                             <div class="client-img">
                                 <img src="<?php echo base_url(); ?>asset/images/portfolio-new/susin-bg.png"
                                     class="portfolio-img" alt="Portfolio-Img">
-                                    <div class="tag-details">
+                                <div class="tag-details">
                                     <a href="javascript:void(0);">Custom Software</a>
                                     <a href="javascript:void(0);">eCommerce</a>
                                     <a href="javascript:void(0);">Mobile App</a>
                                     <a href="javascript:void(0);">Tech Capabilities</a>
                                 </div>
                             </div>
-                            <h4 class="title">Empowering India’s Automation with Fully Manufactured Pneumatic Actuators
-                                by Susin I-Tork</h4>
+                            <a href="javascript:void(0);">
+                                <h4 class="title">Empowering India’s Automation with Fully Manufactured Pneumatic
+                                    Actuators
+                                    by Susin I-Tork</h4>
+                            </a>
                         </a>
                     </div>
                 </div>
@@ -176,15 +121,18 @@
                             <div class="client-img">
                                 <img src="<?php echo base_url(); ?>asset/images/portfolio-new/mayflower-bg-img.png"
                                     class="portfolio-img" alt="Portfolio-Img">
-                                    <div class="tag-details">
+                                <div class="tag-details">
                                     <a href="javascript:void(0);">Custom Software</a>
                                     <a href="javascript:void(0);">eCommerce</a>
                                     <a href="javascript:void(0);">Mobile App</a>
                                     <a href="javascript:void(0);">Tech Capabilities</a>
                                 </div>
                             </div>
-                            <h4 class="title">Delivering Exceptional Apartments & Villas with Unparalleled Quality in
-                                Coimbatore</h4>
+                            <a href="javascript:void(0);">
+                                <h4 class="title">Delivering Exceptional Apartments & Villas with Unparalleled Quality
+                                    in
+                                    Coimbatore</h4>
+                            </a>
                         </a>
                     </div>
                 </div>
@@ -205,8 +153,11 @@
                                     <a href="javascript:void(0);">Tech Capabilities</a>
                                 </div>
                             </div>
-                            <h4 class="title">Empowering India’s Automation with Fully Manufactured Pneumatic Actuators
-                                by Susin I-Tork</h4>
+                            <a href="javascript:void(0);">
+                                <h4 class="title">Empowering India’s Automation with Fully Manufactured Pneumatic
+                                    Actuators
+                                    by Susin I-Tork</h4>
+                            </a>
                         </a>
                     </div>
                 </div>
@@ -214,55 +165,13 @@
         </div>
     </section>
 
-
-
-
     <?php include('footer.php'); ?>
-    <script>
-        setTimeout(function () {
-            $(".alert").fadeOut("slow", function () {
-                $(".alert").remove();
-            });
-        }, 6000);
-    </script>
+
     <script src="<?php echo base_url(); ?>asset/js/bootstrap.min.js"></script>
     <script src="<?php echo base_url(); ?>asset/js/jquery.fancybox.js"></script>
     <script src="<?php echo base_url(); ?>asset/js/mixitup.js"></script>
     <script src="<?php echo base_url(); ?>asset/js/script.js"></script>
 
-    <script>
-        $(document).ready(function () {
-            setTimeout(function () {
-                $('.portfolio-card-container').each(function (index) {
-                    const card = $(this);
-                    setTimeout(function () {
-                        card.addClass('show');
-                    }, index * 100);
-                });
-            }, 100);
-            $('.filter-btn').on('click', function () {
-                $('.filter-btn').removeClass('active');
-                $(this).addClass('active');
-                const filterValue = $(this).data('filter');
-                $('#portfolio-grid > div').each(function (index) {
-                    const item = $(this);
-                    const card = item.find('.portfolio-card-container');
-                    const categories = item.data('categories').split(',');
-                    card.removeClass('show').addClass('hide-item');
-                    setTimeout(() => {
-                        if (filterValue === 'all' || categories.includes(filterValue)) {
-                            item.removeClass('hide');
-                            setTimeout(() => {
-                                card.removeClass('hide-item').addClass('show');
-                            }, index * 100);
-                        } else {
-                            item.addClass('hide');
-                        }
-                    }, 500);
-                });
-            });
-        });
-    </script>
 </body>
 
 </html>
--- asset/js/script.js
+++ asset/js/script.js
@@ -1,1 +1,65 @@
-!function(e){"use strict";e(".filter-list").length&&e(".filter-list").mixItUp({}),e(".single-item-carousel").length&&e(".single-item-carousel").owlCarousel({animateOut:"fadeOut",animateIn:"fadeIn",loop:!0,margin:0,nav:!0,smartSpeed:500,autoplay:6e3,navText:['<span class="fa fa-angle-left"></span>','<span class="fa fa-angle-right"></span>'],responsive:{0:{items:1},600:{items:1},800:{items:1},1024:{items:1},1200:{items:1},1500:{items:1}}}),e(".lightbox-image").length&&e(".lightbox-image").fancybox({openEffect:"fade",closeEffect:"fade",helpers:{media:{}}})}(window.jQuery);
+!(function (e) {
+  "use strict";
+  e(".filter-list").length && e(".filter-list").mixItUp({}),
+    e(".single-item-carousel").length &&
+      e(".single-item-carousel").owlCarousel({
+        animateOut: "fadeOut",
+        animateIn: "fadeIn",
+        loop: !0,
+        margin: 0,
+        nav: !0,
+        smartSpeed: 500,
+        autoplay: 6e3,
+        navText: [
+          '<span class="fa fa-angle-left"></span>',
+          '<span class="fa fa-angle-right"></span>',
+        ],
+        responsive: {
+          0: { items: 1 },
+          600: { items: 1 },
+          800: { items: 1 },
+          1024: { items: 1 },
+          1200: { items: 1 },
+          1500: { items: 1 },
+        },
+      }),
+    e(".lightbox-image").length &&
+      e(".lightbox-image").fancybox({
+        openEffect: "fade",
+        closeEffect: "fade",
+        helpers: { media: {} },
+      });
+})(window.jQuery);
+
+
+$(document).ready(function () {
+    setTimeout(function () {
+        $('.portfolio-card-container').each(function (index) {
+            const card = $(this);
+            setTimeout(function () {
+                card.addClass('show');
+            }, index * 100);
+        });
+    }, 100);
+    $('.filter-btn').on('click', function () {
+        $('.filter-btn').removeClass('active');
+        $(this).addClass('active');
+        const filterValue = $(this).data('filter');
+        $('#portfolio-grid > div').each(function (index) {
+            const item = $(this);
+            const card = item.find('.portfolio-card-container');
+            const categories = item.data('categories').split(',');
+            card.removeClass('show').addClass('hide-item');
+            setTimeout(() => {
+                if (filterValue === 'all' || categories.includes(filterValue)) {
+                    item.removeClass('hide');
+                    setTimeout(() => {
+                        card.removeClass('hide-item').addClass('show');
+                    }, index * 100);
+                } else {
+                    item.addClass('hide');
+                }
+            }, 500);
+        });
+    });
+});
