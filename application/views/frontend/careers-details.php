29373f36d64b75f5b98cc4b61ccf11f757f2a0c1
karthikeyanraj02 <karthikeyanraj.r@gmail.com>
2024-12-10 18:01:22 +0530

    career-detail page

--- application/views/frontend/careers-details.php
+++ application/views/frontend/careers-details.php
@@ -74,161 +74,190 @@
 	</div>
 
 	<?php if ($career_details) {  ?>
-	<section class="career-section">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12 col-sm-12">
-					<?php echo message_box('success'); ?>
-					<?php echo message_box('error'); ?>
-					<div class="job-verticle-list">
-						<div class="vertical-job-card">
-							<div class="vertical-job-header">
-								<div class="vrt-job-cmp-logo">
-									<a href="#"><i class="<?php echo $career_details->icon; ?> pt-7 job-icon"
-											aria-hidden="true"></i></a>
-								</div>
-								<h4><a href="#"><?php echo $career_details->title; ?></a></h4>
-								<span class="pull-right vacancy-no">Number of Vacancy <span
-										class="v-count"><?php echo $career_details->no_of_vacancy; ?></span></span>
-							</div>
-							<div class="vertical-job-body">
-								<div class="row">
-									<div class="col-md-9 col-sm-8">
-										<ul class="can-skils">
-											<li class="mb-2"><strong>Job Id: </strong><?php echo $career_details->job_id; ?></li>
-											<li class="mb-2"><strong>Job Type: </strong><?php echo $career_details->job_type; ?></li>
-											<?php if (!empty($career_details->job_type)) { ?>
-											<li class="mb-2"><strong>Qualification:
-												</strong><?php echo $career_details->qualification; ?></li>
-											<?php } ?>
-											<li class="mb-2"><strong>Skills: </strong>
-												<?php
-													$skills = explode(',', $career_details->skills);
-													foreach ($skills as $skill) {
-													?>
-												<span class="skill-tag"><?php echo $skill; ?></span>
-												<?php } ?>
-											</li>
-											<li class="mb-2"><strong>Experience: </strong><?php echo $career_details->experience; ?>
-											</li>
-											<li class="mb-2"><strong>Location: </strong><?php echo $career_details->location; ?></li>
-										</ul>
-									</div>
-								</div>
-							</div>
-							<?php if (!empty($career_details->job_description)) { ?>
-							<div class="vertical-job-footer">
-								<div class="row">
-									<div class="col-md-12 col-sm-12">
-										<?php echo $career_details->job_description; ?>
-									</div>
-								</div>
-							</div>
-							<?php } ?>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12 col-sm-12">
-					<form method="post" id="FormValidation" action="<?php echo base_url('careerform') ?>"
-						enctype="multipart/form-data">
+		<section class="career-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 col-sm-12">
+						<?php echo message_box('success'); ?>
+						<?php echo message_box('error'); ?>
 						<div class="job-verticle-list">
 							<div class="vertical-job-card">
 								<div class="vertical-job-header">
-									<h4><a href="#">Submit Your Application</a></h4>
+									<div class="job-header-new">
+										<div class="vrt-job-cmp-logo">
+											<a href="#"><i class="<?php echo $career_details->icon; ?> job-icon"
+													aria-hidden="true"></i></a>
+										</div>
+										<div class="d-flex flex-column gp-4">
+											<h4><a href="#"><?php echo $career_details->title; ?></a></h4>
+											<p class="mb-0">Location: </strong><?php echo $career_details->location; ?></p>
+										</div>
+									</div>
+									<button class="btn-job" onclick="scrollToForm()">Apply Now</button>
+									<!-- <span class="pull-right vacancy-no">Number of Vacancy <span
+											class="v-count"><?php echo $career_details->no_of_vacancy; ?></span></span> -->
 								</div>
 								<div class="vertical-job-body">
 									<div class="row">
-										<div class="col-md-9 col-sm-8">
-											<input name="website" class="form-control d-none" style="display:none;"
-												value="">
-											<input type="hidden" name="job_post_id" class="form-control"
-												value="<?php echo $career_details->job_post_id; ?>">
-											<input type="hidden" name="slug" class="form-control"
-												value="<?php echo $career_details->slug; ?>">
-											<input type="hidden" name="job_id" class="form-control"
-												value="<?php echo strtolower($career_details->job_id); ?>">
-											<div class="form-group">
-												<label>Name<span class="red-text">*</span></label>
-												<input type="text" name="name" class="required form-control"
-													placeholder="Name*" value="<?php echo set_value('name'); ?>">
-												<div class="red-text">
-													<?php echo form_error('name'); ?>
-												</div>
+										<div class="col-md-12 col-sm-8">
+											<ul class="can-skils">
+												<li class="mb-2"><strong>Job Id : </strong><?php echo $career_details->job_id; ?></li>
+												<li class="mb-2"><strong>Job Type : </strong><?php echo $career_details->job_type; ?></li>
+												<?php if (!empty($career_details->job_type)) { ?>
+													<li class="mb-2"><strong>Qualification :
+														</strong><?php echo $career_details->qualification; ?></li>
+												<?php } ?>
+												<li class="mb-2 d-flex align-items-center mtb-5"><strong>Skills : </strong>
+													<div>
+														<?php
+														$skills = explode(',', $career_details->skills);
+														foreach ($skills as $skill) {
+														?>
+															<span class="skill-tag"><?php echo $skill; ?></span>
+														<?php } ?>
+													</div>
+												</li>
+												<li class="mb-2"><strong>Experience : </strong><?php echo $career_details->experience; ?>
+												</li>
+												<li class="mb-2"><strong>Location : </strong><?php echo $career_details->location; ?></li>
+											</ul>
+										</div>
+									</div>
+								</div>
+								<?php if (!empty($career_details->job_description)) { ?>
+									<div class="vertical-job-footer">
+										<div class="row">
+											<div class="col-md-12 col-sm-12">
+												<?php echo $career_details->job_description; ?>
 											</div>
-											<div class="form-group">
-												<label>Email<span class="red-text">*</span></label>
-												<input type="email" name="email" class="required form-control"
-													placeholder="Email*" value="<?php echo set_value('email'); ?>">
-												<div class="red-text">
-													<?php echo form_error('email'); ?>
-												</div>
-											</div>
-											<div class="form-group">
-												<label>Phone<span class="red-text">*</span></label>
-												<input type="text" name="mobile" class="required form-control"
-													placeholder="Phone *" value="<?php echo set_value('mobile'); ?>">
-												<div class="red-text">
-													<?php echo form_error('mobile'); ?>
-												</div>
-											</div>
-											<div class="form-group">
-												<label>Experience<span class="red-text">*</span></label>
-												<input type="text" name="experience" class="required form-control"
-													placeholder="Experience *"
-													value="<?php echo set_value('experience'); ?>">
-												<div class="red-text">
-													<?php echo form_error('experience'); ?>
-												</div>
-											</div>
-											<div class="form-group">
-												<label>Upload CV<span class="red-text">*</span></label>
-												<div class="custom-file-upload">
-													<input type="file" class="required " name="resume" />
-												</div>
-												<div class="red-text">
-													<?php echo form_error('resume'); ?>
-												</div>
-											</div>
-											<div class="form-group">
-												<label>Cover Letter<span class="red-text">*</span></label>
-												<textarea class="form-control height-120 required " name="message"
-													placeholder="Cover Letter *"><?php echo set_value('message'); ?></textarea>
-												<div class="red-text">
-													<?php echo form_error('message'); ?>
+										</div>
+									</div>
+								<?php } ?>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row" id="form-section">
+					<div class="col-md-12 col-sm-12">
+						<form method="post" id="FormValidation" action="<?php echo base_url('careerform') ?>"
+							enctype="multipart/form-data">
+							<div class="job-verticle-list">
+								<div class="vertical-job-card">
+									<div class="vertical-job-header">
+										<h4><a href="#">Submit Your Application</a></h4>
+									</div>
+									<div class="vertical-job-body">
+										<div class="row">
+											<div class="col-md-12 col-sm-8">
+												<input name="website" class="form-control d-none" style="display:none;"
+													value="">
+												<input type="hidden" name="job_post_id" class="form-control"
+													value="<?php echo $career_details->job_post_id; ?>">
+												<input type="hidden" name="slug" class="form-control"
+													value="<?php echo $career_details->slug; ?>">
+												<input type="hidden" name="job_id" class="form-control"
+													value="<?php echo strtolower($career_details->job_id); ?>">
+
+												<div class="d-flex flex-wrap gpt-20">
+													<div class="form-group col-md-6">
+														<label>Name<span class="red-text">*</span></label>
+														<input type="text" name="name" class="required form-control"
+															placeholder="Name*" value="<?php echo set_value('name'); ?>">
+														<div class="red-text">
+															<?php echo form_error('name'); ?>
+														</div>
+													</div>
+													<div class="form-group col-md-6">
+														<label>Email<span class="red-text">*</span></label>
+														<input type="email" name="email" class="required form-control"
+															placeholder="Email*" value="<?php echo set_value('email'); ?>">
+														<div class="red-text">
+															<?php echo form_error('email'); ?>
+														</div>
+													</div>
+													<div class="form-group col-md-6">
+														<label>Phone<span class="red-text">*</span></label>
+														<input type="text" name="mobile" class="required form-control"
+															placeholder="Phone *" value="<?php echo set_value('mobile'); ?>">
+														<div class="red-text">
+															<?php echo form_error('mobile'); ?>
+														</div>
+													</div>
+													<div class="form-group col-md-6">
+														<label>Experience<span class="red-text">*</span></label>
+														<input type="text" name="experience" class="required form-control"
+															placeholder="Experience *"
+															value="<?php echo set_value('experience'); ?>">
+														<div class="red-text">
+															<?php echo form_error('experience'); ?>
+														</div>
+													</div>
+													<div class="form-group col-md-6">
+														<label>Upload CV<span class="red-text">*</span></label>
+														<div class="custom-file-upload">
+															<input type="file" class="required " name="resume" />
+														</div>
+														<div class="red-text">
+															<?php echo form_error('resume'); ?>
+														</div>
+													</div>
+													<div class="form-group col-md-6 role-select">
+													<label>Role looking for<span class="red-text">*</span></label>
+														<select class="form-select form-control" aria-label="Default select example">
+															<option selected>Select a Role</option>
+															<option value="1">Mobile App Developer</option>
+															<option value="2">Mobile App Developer</option>
+															<option value="3">Mobile App Developer</option>
+														</select>
+													</div>
+													<div class="form-group col-md-12">
+														<label>Cover Letter<span class="red-text">*</span></label>
+														<textarea class="form-control height-120 required " name="message"
+															placeholder="Cover Letter *"><?php echo set_value('message'); ?></textarea>
+														<div class="red-text">
+															<?php echo form_error('message'); ?>
+														</div>
+													</div>
 												</div>
 											</div>
 										</div>
 									</div>
-								</div>
-								<div class="vertical-job-footer">
-									<div class="row">
-										<div class="col-md-12 col-sm-12">
-											<div class="form-group text-center">
-												<button type="submit" class="btn theme-btn btn-m">Apply Now</button>
+									<div class="vertical-job-footer">
+										<div class="row">
+											<div class="col-md-12 col-sm-12">
+												<div class="form-group text-center">
+													<button type="submit" class="btn-job">Submit</button>
+												</div>
 											</div>
 										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-					</form>
+						</form>
+					</div>
 				</div>
 			</div>
-		</div>
-	</section>
+		</section>
 	<?php } ?>
 	<br>
 	<?php include('footer.php'); ?>
 	<script src="<?php echo base_url(); ?>assets/js/jquery.validate.js"></script>
 	<script src="<?php echo base_url(); ?>assets/js/form-validation.js"></script>
 	<script>
-		setTimeout(function () {
-			$(".alert").fadeOut("slow", function () {
+		setTimeout(function() {
+			$(".alert").fadeOut("slow", function() {
 				$(".alert").remove();
 			});
 		}, 6000);
+	</script>
+	<script>
+		function scrollToForm() {
+			const formSection = document.getElementById('form-section');
+			formSection.scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			});
+		}
 	</script>
 	<script src="<?php echo base_url(); ?>asset/js/bootstrap.min.js"></script>
 </body>
