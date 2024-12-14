61629ee8264299b9b0e53c6dab906fe234eb9049
RASHIKA K V <rashika@cloudi5.com>
2024-12-14 17:24:42 +0530

    portfolio section in home page

--- application/views/frontend/portfolio.php
+++ application/views/frontend/portfolio.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>
         <?php if ($meta) {
@@ -26,88 +27,164 @@
     <link rel="canonical" href="<?php echo base_url('portfolio'); ?>" />
     <?php $this->load->view('frontend/common-css'); ?>
     <link rel="stylesheet" href="<?php echo base_url(); ?>asset/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>asset/css/portfolio-new.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>asset/css/owl.theme.default.min.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>asset/css/jquery.fancybox.css">
     <link rel="stylesheet" href="<?php echo base_url(); ?>asset/css/jquery.fancybox.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>asset/css/icon-font.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>asset/css/web-dev.css">
     <?php $this->load->view('frontend/fb-pixel-code'); ?>
 </head>
-<!-- Icon font-->
-<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>asset/css/icon-font.css">
-<body id="cont1">
+
+
+
+<body id="cont1" class="body-inner">
+
     <?php include('header.php'); ?>
-    <div class="banner-area about-banner resp-banner" id="banner-area" alt="Portfolio">
-        <div class="container">
+
+    <!-- <div class="banner-area overlay about-banner resp-banner common-inner-banner" id="banner-area"
+        alt="Web Development Company in Coimbatore" title="Website Development">
+        <div class="inner-pattern"> <img
+                src="<?php echo base_url(); ?>asset/images/home/svg/common-inner-banner-pattern.svg"
+                alt="banner-pattern">
+        </div>
+        <div class="container banner-content">
             <div class="row justify-content-center">
-                <div class="col">
-                    <div class="banner-heading">
-                        <h1 class="banner-title2">Portfolio</h1>
-                        <ol class="breadcrumb">
-                            <li><a href="<?php echo base_url(); ?>">Home</a></li>
-                            <li>Portfolio</li>
-                        </ol>
+                <div class="shape-wrapper">
+                    <div class="shape-tri-1">
+                        <img src="<?php echo base_url(); ?>asset/images/home/banner_shape02.png" alt="Shape-1">
+                    </div>
+                    <div class="shape-tri-3">
+                        <img src="<?php echo base_url(); ?>asset/images/web-development/bg-blue.webp" alt="Shape-3">
+                    </div>
+                    <div class="shape-tri-4">
+                        <img src="<?php echo base_url(); ?>asset/images/web-development/bg-blue.webp" alt="Shape-4">
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-    <!-- Portfolio -->
-    <section>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 col-sm-12">
-                    <section class="projects-page-section">
-                        <div class="auto-container">
-                            <div class="mixitup-gallery">
-                                <div class="filters clearfix">
-                                    <ul class="filter-tabs filter-btns text-center clearfix">
-                                        <li class="filter active" data-role="button" data-filter="all">All</li>
-                                        <?php foreach ($category as $res_category) { ?>
-                                            <li class="filter" data-role="button" data-filter=".<?php echo strtolower(str_replace(' ', '_', $res_category->title)); ?>">
-                                                <?php echo ucwords($res_category->title); ?></li>
-                                        <?php } ?>
-                                    </ul>
-                                </div>
-                                <div class="filter-list row clearfix" id="MixItUpC72ED9">
-                                    <?php foreach ($portfolio as $res_portfolio) { ?>
-                                        <div class="gallery-item-two mix <?php echo strtolower(str_replace(' ', '_', $res_portfolio->category)); ?> col-lg-4 col-md-6 col-sm-12 d-inlineb">
-                                            <div class="inner-box">
-                                                <figure class="image-box">
-                                                    <img src="<?php echo base_url() . $res_portfolio->thumbnail; ?>" alt="">
-                                                    <div class="overlay-box">
-                                                        <div class="overlay-inner">
-                                                            <div class="content">
-                                                                <a href="<?php echo base_url() . $res_portfolio->thumbnail_popup; ?>" data-fancybox="gallery-3" data-caption="" class="link"><span class="fa fa-search-plus"></span></a>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </figure>
-                                                <div class="lower-box">
-                                                    <h3><a href="<?php echo base_url() . 'portfolio/' . $res_portfolio->slug . '-' . $res_portfolio->id; ?>">
-                                                            <?php echo ucwords($res_portfolio->title); ?></a></h3>
-                                                    <div class="category"><?php echo $res_portfolio->services; ?></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    <?php } ?>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
+                <div class="col-md-10">
+                    <div class="banner-tagline">
+                        <span> <img src="<?php echo base_url(); ?>asset/images/web-development/star.png"
+                                alt="Convert into Mockups"> Transform Your Vision into Digital Success!</span>
+                    </div>
+                    <h2 class="title">Gear up your business returns with top-performing <span>Web Development</span>
+                        Company in Coimbatore!</h2>
                 </div>
             </div>
         </div>
+    </div> -->
+
+    <section class="portfolio-new-section">
+        <div class="container">
+            <div class="row justify-center">
+                <div class="col-lg-12 text-center">
+                    <h2 class="portfolio-title mb-3">
+                        We deliver a diverse array of
+                        <span class="animated-text">high-quality products</span>
+                    </h2>
+                    <p class="portfolio-desc">Here are some of our standout creations</p>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-12 text-center portfolio-filters">
+                    <button class="btn filter-btn active" data-filter="all">All</button>
+                    <?php foreach ($category as $res_category) { ?>
+                    <button class="btn filter-btn"
+                        data-filter=".<?php echo strtolower(str_replace(' ', '_', $res_category->title)); ?>">
+                        <?php echo ucwords($res_category->title); ?>
+                    </button>
+                    <?php } ?>
+                </div>
+
+            </div>
+            <div class="row" id="portfolio-grid">
+                <?php foreach ($portfolio as $res_portfolio) { ?>
+                <div class="col-md-6"
+                    data-categories="<?php echo strtolower(str_replace(' ', '_', $res_portfolio->category)); ?>">
+                    <a href="javascript:void(0);">
+                        <div class="portfolio-card-container">
+                            <div class="client-logo">
+                                <!-- <img src="<?php echo base_url(); ?>asset/images/portfolio-new/mayflower-logo.png"
+                                    class="client-logo-img" alt="Clients-Logo"> -->
+                                    <img src="<?php echo base_url() . $res_portfolio->thumbnail; ?>" class="client-logo-img" alt="Clients-Logo">
+
+                            </div>
+                            <div class="client-img">
+                                <!-- <img src="<?php echo base_url(); ?>asset/images/portfolio-new/mayflower-bg-img.png"
+                                    class="portfolio-img" alt="Portfolio-Img"> -->
+                                    <img src="<?php echo base_url() . $res_portfolio->thumbnail_popup; ?>" class="portfolio-img" alt="Portfolio-Img">
+
+                                <div class="tag-details">
+                                    <a><?php echo $res_portfolio->services; ?></a>
+                                </div>
+                            </div>
+                            <h4>
+                                <a class="title"
+                                    href="<?php echo base_url() . 'portfolio/' . $res_portfolio->slug . '-' . $res_portfolio->id; ?>">
+                                    <?php echo ucwords($res_portfolio->title); ?></a>
+                            </h4>
+                        </div>
+                    </a>
+                </div>
+                <?php } ?>
+            </div>
+
+
+        </div>
     </section>
+
     <?php include('footer.php'); ?>
     <script>
-        setTimeout(function() {
-            $(".alert").fadeOut("slow", function() {
-                $(".alert").remove();
-            });
-        }, 6000);
+    setTimeout(function() {
+        $(".alert").fadeOut("slow", function() {
+            $(".alert").remove();
+        });
+    }, 6000);
     </script>
     <script src="<?php echo base_url(); ?>asset/js/bootstrap.min.js"></script>
     <script src="<?php echo base_url(); ?>asset/js/jquery.fancybox.js"></script>
     <script src="<?php echo base_url(); ?>asset/js/mixitup.js"></script>
     <script src="<?php echo base_url(); ?>asset/js/script.js"></script>
+
+    <script>
+    $(document).ready(function() {
+        setTimeout(function() {
+            $('.portfolio-card-container').each(function(index) {
+                const card = $(this);
+                setTimeout(function() {
+                    card.addClass('show');
+                }, index * 100);
+            });
+        }, 100);
+
+        $('.filter-btn').on('click', function() {
+            $('.filter-btn').removeClass('active');
+            $(this).addClass('active');
+            const filterValue = $(this).data('filter'); // e.g., ".web_design"
+            $('#portfolio-grid > div').each(function(index) {
+                const item = $(this);
+                const card = item.find('.portfolio-card-container');
+                const categories = item.data('categories').split(
+                    ' '); // Space-separated categories
+
+                console.log('Filter Value:', filterValue);
+                console.log('Categories:', categories);
+
+                card.removeClass('show').addClass('hide-item');
+                setTimeout(() => {
+                    if (filterValue === 'all' || categories.includes(filterValue
+                            .substring(1))) { // compare without the dot
+                        item.removeClass('hide');
+                        setTimeout(() => {
+                            card.removeClass('hide-item').addClass('show');
+                        }, index * 100);
+                    } else {
+                        item.addClass('hide');
+                    }
+                }, 500);
+            });
+        });
+    });
+    </script>
 </body>
+
 </html>
